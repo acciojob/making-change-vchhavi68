a6de2c6c950c67f6f0378c61a4b5135546060af4
vchhavi68 <vchhavi68@gmail.com>
2025-12-29 16:49:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	let amount = Number(c);
+
+  const q = Math.floor(amount / 25);
+  amount %= 25;
+
+  const d = Math.floor(amount / 10);
+  amount %= 10;
+
+  const n = Math.floor(amount / 5);
+  amount %= 5;
+
+  const p = amount;
+
+  return { q, d, n, p };
 };
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
